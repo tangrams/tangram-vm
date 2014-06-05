ed87c0f416692ef02bfe064e220bc6cfe469fb90
Peter Richardson <m33tar@gmail.com>
2014-06-04 22:17:15 -0400

    renamed puppet.pp to precise.pp

---
+++ manifests/precise.pp
@@ -0,0 +1,140 @@
+exec { "apt-update":
+  command => "/usr/bin/apt-get update"
+}
+
+# Ensure apt-get update has been run before installing any packages
+Exec["apt-update"] -> Package <| |>
+
+package {
+    "build-essential":
+        ensure => installed,
+        provider => apt;
+    "python":
+        ensure => installed,
+        provider => apt;
+    "python-dev":
+        ensure => installed,
+        provider => apt;
+    "python-setuptools":
+        ensure => installed,
+        provider => apt;
+    "python-software-properties":
+        ensure => installed,
+        provider => apt;
+}
+
+package { "python-pip": 
+  ensure => latest,
+}
+
+exec { "ppa:ubuntugis":
+  command => "/usr/bin/add-apt-repository ppa:ubuntugis && /usr/bin/apt-get update",
+  require => Package['python-software-properties'],
+}
+
+package {
+	"libgdal1":
+		ensure => latest,
+		provider => apt;
+	"postgresql-9.3":
+		ensure => latest,
+		provider => apt;
+	"postgresql-9.3-postgis-2.1":
+		ensure => latest,
+		provider => apt;
+	"postgresql-client-9.3":
+		ensure => latest,
+		provider => apt;
+	"protobuf-compiler":
+		ensure => latest,
+		provider => apt;
+	"libprotobuf-c0-dev":
+		ensure => latest,
+		provider => apt;
+	"protobuf-c-compiler":
+		ensure => latest,
+		provider => apt;
+	"libprotobuf-dev":
+		ensure => latest,
+		provider => apt;
+	"libtokyocabinet-dev":
+		ensure => latest,
+		provider => apt;
+	"python-psycopg2":
+		ensure => latest,
+		provider => apt;
+	"libgeos-c1":
+		ensure => latest,
+		provider => apt;
+	"postgresql-contrib":
+		ensure => latest,
+		provider => apt;
+}
+
+exec { "createuser":
+  command => "sudo su -p - postgres -c \"createuser osm -s -d\""
+}
+exec { "createdb":
+  command => "sudo su -p - postgres -c \"createdb -O osm osm\""
+}
+exec { "postgres-extension:hstore":
+  command => "sudo su -p - postgres -c 'psql -d osm -c \"CREATE EXTENSION hstore;\"'"
+}
+exec { "postgres-extension:adminpack":
+  command => "sudo su -p - postgres -c 'psql -d osm -c \"CREATE EXTENSION adminpack;\"'"
+}
+exec { "postgres-extension:postgis":
+  command => "sudo su -p - postgres -c 'psql -d osm -c \"CREATE EXTENSION postgis;\"'"
+}
+
+
+package {
+	"git-core": 
+		ensure => latest,
+		provider => apt;
+	"dh-autoreconf": 
+		ensure => latest,
+		provider => apt;
+	"libxml2-dev": 
+		ensure => latest,
+		provider => apt;
+	"libbz2-de": 
+		ensure => latest,
+		provider => apt;
+	"libgeos-dev": 
+		ensure => latest,
+		provider => apt;
+	"libproj-dev": 
+		ensure => latest,
+		provider => apt;
+	"libpq-dev": 
+		ensure => latest,
+		provider => apt;
+	"libgeos++-dev": 
+		ensure => latest,
+		provider => apt;
+}
+
+package {
+	"shapely": 
+		ensure => latest,
+		provider => pip,
+		require => Package['python-pip'],
+	"PIL": 
+		ensure => latest,
+		provider => pip,
+		require => Package['python-pip'],
+	"modestmaps": 
+		ensure => latest,
+		provider => pip,
+		require => Package['python-pip'],
+	"simplejson": 
+		ensure => latest,
+		provider => pip,
+		require => Package['python-pip'],
+	"werkzeug": 
+		ensure => latest,
+		provider => pip,
+		require => Package['python-pip'],
+}
+
