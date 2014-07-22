ba320db57842c98413858c7f0490978e6337ee71
Peter Richardson <github@zoomy.net>
2014-07-22 12:03:34 -0400

    simplified setup method

--- install2.sh
+++ install2.sh
@@ -1,5 +1,5 @@
 #!bin/bash
 git clone git@github.com:meetar/vector-map.git
 cd vector-map
-echo "\n Starting web server -- test it at http://localhost:9000/#mapzen"
+echo -e "\n Starting web server -- test it at http://localhost:9000/#mapzen"
 python -m SimpleHTTPServer 9000
