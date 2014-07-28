83f6451988add389af78fe4c292f7d452032f76b
Peter Richardson <github@zoomy.net>
2014-07-28 14:55:28 -0400

    changed link to reflect repo rename

--- install2.sh
+++ install2.sh
@@ -1,5 +1,5 @@
 #!bin/bash
 git clone git@github.com:meetar/tangram.git
-cd vector-map
+cd tangram
 echo -e "\n Starting web server -- test it at http://localhost:9000/#mapzen"
 python -m SimpleHTTPServer 9000
