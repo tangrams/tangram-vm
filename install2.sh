4d95fce7a4b4e0b7e6082f7a6434b4ac6779fd05
Peter Richardson <github@zoomy.net>
2014-08-06 13:50:20 -0400

    switched repo link to bcamper, added make step

--- install2.sh
+++ install2.sh
@@ -1,5 +1,7 @@
 #!bin/bash
-git clone git@github.com:meetar/tangram.git
+git clone git@github.com:bcamper/tangram.git
 cd tangram
+sudo npm install
+make
 echo -e "\n Starting web server -- test it at http://localhost:9000/#mapzen"
 python -m SimpleHTTPServer 9000
